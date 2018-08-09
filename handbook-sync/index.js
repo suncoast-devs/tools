d6566fbebe0590d0d903b918cd9f01cf6634f3e3
Jason L Perry <jason@suncoast.io>
2018-08-09 11:10:32 -0400

    Sync!

--- handbook-sync/index.js
+++ handbook-sync/index.js
@@ -1,10 +1,54 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!Object} req HTTP request context.
- * @param {!Object} res HTTP response context.
- */
-exports.handbookSync = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!'
-  res.status(200).send(message)
+const got = require('got')
+
+const client = got.extend({
+  baseUrl: 'https://api.github.com',
+  json: true,
+  headers: {
+    Authorization: `token ${process.env.AUTH_TOKEN}`
+  }
+})
+
+exports.handbookSync = async (req, res) => {
+  try {
+    const {
+      commit: { sha: parentSha }
+    } = (await client.get('/repos/suncoast-devs/web/branches/master')).body
+    const {
+      commit: { sha: targetSha }
+    } = (await client.get('/repos/suncoast-devs/handbook/branches/master')).body
+
+    const res = await client.post('/repos/suncoast-devs/web/git/trees', {
+      body: {
+        base_tree: parentSha,
+        tree: [
+          {
+            path: 'data/handbook',
+            mode: '160000',
+            type: 'commit',
+            sha: targetSha
+          }
+        ]
+      }
+    })
+
+    const { sha: treeSha } = res.body
+
+    const { sha: commitSha } = (await client.post('/repos/suncoast-devs/web/git/commits', {
+      body: {
+        message: 'Synchronize Handbook',
+        tree: treeSha,
+        parents: [parentSha]
+      }
+    })).body
+
+    await client.patch('/repos/suncoast-devs/web/git/refs/heads/master', {
+      body: {
+        sha: commitSha
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
+
+exports.handbookSync()
